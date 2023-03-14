e9437f10590958dacea1fb229b71a9ec8b1535ac
EC2 Default User <ec2-user@ip-172-31-4-28.ap-northeast-1.compute.internal>
2023-03-14 11:10:51 +0000

    top作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :books
   get 'books/new'
   get 'books/index'
   get 'books/show'
--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,6 @@
 ActiveRecord::Schema.define(version: 2023_03_13_085715) do
 
   create_table "books", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
